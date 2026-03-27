4891fe47a43e79b535eb14367693f8f860b57454
offeck <offeck100@gmail.com>
2026-03-27 18:22:14 +0300

    Refactor string processing functions and implement vowel counting and palindrome checking

--- work1/ex1_template/src/part2.ts
+++ work1/ex1_template/src/part2.ts
@@ -2,18 +2,20 @@ import * as R from "ramda";
 
 const stringToArray = R.split("");
 
+const normalizeString = R.pipe(R.toLower, R.replace(/\W/g, ""));
+
 /* Question 2.1 */
-const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+const vowels: string[] = ["a", "e", "i", "o", "u"];
+export const countVowels: (s: string) => number = (s: string) => stringToArray(normalizeString(s)).filter((c) => vowels.includes(c)).length;
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
-  
+export const isPalindrome: (text: string) => boolean = (text) => stringToArray(normalizeString(text)).every((a, i, arr) => a === arr[arr.length - 1 - i]);
 
 /* Question 2.3 */
 export type WordTree = {
-    root: string;
-    children: WordTree[];
-}
+  root: string;
+  children: WordTree[];
+};
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence = (t: WordTree): string =>
+  [t.root, ...t.children.map(treeToSentence)].filter((s) => s !== "").join(" ");
