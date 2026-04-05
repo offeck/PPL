afc3e9bf29a1230d3c3b622ca4478c80c8a1f121
offeck <offeck100@gmail.com>
2026-04-05 15:32:08 +0300

    Refactor findResult and related functions for improved clarity and functionality

--- work1/ex1_template/src/part3.ts
+++ work1/ex1_template/src/part3.ts
@@ -2,24 +2,32 @@ import { Result, makeFailure, makeOk, bind, either } from "./lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
-    for (let i = 0; i < a.length; i++) {
-        if (pred(a[i])) return a[i];
-    }
-    throw "No element found.";
-}
+  for (let i = 0; i < a.length; i++) {
+    if (pred(a[i])) return a[i];
+  }
+  throw "No element found.";
+};
 
-export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => undefined as any;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => (a.filter(pred).length === 0 ? makeFailure("No element found.") : makeOk(a.filter(pred)[0]));
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
-    try {
-        const x = findOrThrow(x => x % 2 === 0, a);
-        return x * x;
-    } catch (e) {
-        return -1;
-    }
-}
+  try {
+    const x = findOrThrow((x) => x % 2 === 0, a);
+    return x * x;
+  } catch (e) {
+    return -1;
+  }
+};
 
-export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => undefined as any;
-export const returnSquaredIfFoundEven_v3 = (a: number[]): number => undefined as any;
-
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> =>
+  bind(
+    findResult((x: number) => x % 2 === 0, a),
+    (x: number) => makeOk(x * x),
+  );
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number =>
+  either(
+    findResult((x: number) => x % 2 === 0, a),
+    (x: number) => x * x,
+    (_message: string) => -1,
+  );
